cf708bc590d3f114c06c84ff139fa12a13ea9095
vassin <vassin@kolesa.kz>
2023-05-29 09:38:09 +0600

    First commit

--- store/events.ts
+++ store/events.ts
@@ -13,7 +13,9 @@ export const useEventsStore = defineStore('events', {
                 const { data } = await axios.post('https://onsite.iteca.kz/exh-json/', {
                     apiKey: '4b4277bd1523915d0655ecc44992f2db',
                     lang: 'ru',
+                    projectID: 0,
                     recCount: 5,
+                    lastMonth: 0,
                 });
 
                 console.log('data1', data);
@@ -24,7 +26,9 @@ export const useEventsStore = defineStore('events', {
             const { data } = await axios.post('https://onsite.iteca.kz/exh-json/', {
                 apiKey: '4b4277bd1523915d0655ecc44992f2db',
                 lang: 'ru',
+                projectID: 0,
                 recCount: 5,
+                lastMonth: 0,
             });
 
             console.log('data2', data);
